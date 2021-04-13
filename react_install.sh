501a8290707e2a90dd7fc486c0429e5aac5f45fb
Kento.Yamada <masterhinagiku@gmail.com>
2021-04-13 20:50:22 +0900

    React Install Script npm Add

--- react_install.sh
+++ react_install.sh
@@ -5,4 +5,8 @@ nvm install node && \
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf_backup && \
 sudo cp ./nginx.conf /etc/nginx/nginx.conf && \
 sudo systemctl restart nginx
+cd quick-react && \
+npm install && \
+npm build && \
+npm run start && \
 echo "React Setup Complete!!"
