d5254e217e62915107f9b31941ca1b91b0e2408b
Kento.Yamada <masterhinagiku@gmail.com>
2021-04-13 20:42:39 +0900

    React Install Script Sudo Add

--- react_install.sh
+++ react_install.sh
@@ -2,7 +2,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash && \
 . ~/.nvm/nvm.sh && \
 nvm install node && \
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf_backup && \
-cp ./nginx.conf /etc/nginx/nginx.conf && \
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf_backup && \
+sudo cp ./nginx.conf /etc/nginx/nginx.conf && \
 sudo systemctl restart nginx
 echo "React Setup Complete!!"
