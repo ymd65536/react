3cf41f4dcc5d833f35b9a42b37a22be588791c5b
Kento.Yamada <masterhinagiku@gmail.com>
2021-04-13 21:41:51 +0900

    React Install Script modifeid

--- react_install.sh
+++ react_install.sh
@@ -1,6 +1,4 @@
 # !/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash && \
-. ~/.nvm/nvm.sh && \
 cd quick-react && \
 nvm install node && \
 npm install && \
